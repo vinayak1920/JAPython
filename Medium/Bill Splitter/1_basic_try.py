7451098fdc8c33825721d27cb998c3d56d1d373c
vinayak-kamath <vinayak1920@gmail.com>
2021-08-23 21:07:55 +0530

    Create 1_basic_try.py

---
+++ Medium/Bill Splitter/1_basic_try.py
@@ -0,0 +1,19 @@
+# write your code here
+friends = {}
+try:
+    num_friends = int(input('Enter the number of friends joining (including you):\n'))
+    assert num_friends > 0
+except Exception:
+    print("\nNo one is joining for the party")
+else:
+    print('\nEnter the name of every friend (including you), each on a new line:')
+    for i in range(num_friends):
+        friends_name = input()
+        friends[friends_name] = 0
+    bill = float(input('\nEnter the total bill value:\n'))
+    split_bill = bill / num_friends
+    if split_bill % 1 == 0:
+        split_bill = int(split_bill)
+    for k in friends.keys():
+        friends[k] = round(split_bill, 2)
+    print(friends)
